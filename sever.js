403aeb9e360e8525c2f62bc76d9ee62988e74a37
Justin <justin@vinova.com.sg>
2023-02-23 09:37:13 +0700

    init

---
+++ sever.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const sever = http.createServer((req, res) => {
+	console.log('run request...');
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<h3>Hello world! </h3>');
+	res.end();
+});
+sever.listen(3000, 'localhost', () => {
+	console.log('Node.Js server is running on port:3000');
+});
